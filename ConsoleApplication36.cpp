bd4aa9ec9ba9809a01d012007aef66688fcdd980
fir1zix228 <fir1zixx@gmail.com>
2025-02-13 16:27:20 +0300

    Add files via upload

---
+++ ConsoleApplication36.cpp
@@ -0,0 +1,38 @@
+﻿#include <cfloat>
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    setlocale(LC_ALL, "Rus");
+    char op;
+    double a, b, res;
+
+
+    cout << "Введите действие (+, -, *, /): ";
+    cin >> op;
+    cout << "Введите два числа: ";
+    cin >> a >> b;
+    switch (op) {
+    case '+':
+        res = a + b;
+        break;
+
+    case '-':
+        res = a - b;
+        break;
+    case '*':
+        res = a * b;
+        break;
+    case '/':
+        res = a / b;
+        break;
+    default:
+        cout << "Ошибка! Оператор не правильный";
+        res = -DBL_MAX;
+    }
+    if (res != -DBL_MAX)
+        cout << "Результат: " << res;
+    return 0;
+}
+
